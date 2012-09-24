4b93f757b631c1e796a47c2ca130424bd2f1b9d2
Hernani Borges de Freitas <hernani@acquia.com>
2012-09-24 10:00:31 +0100

    added deploy-to as part of server configuration

--- sitealias-builder.php
+++ sitealias-builder.php
@@ -6,21 +6,21 @@ function deploy_create_aliases ($deploy_sitename, $deploy_repository, $deploy_en
       return array();
 
     foreach ($deploy_environments as $env_key => $environment) {
-      foreach ($environment['servers'] as $server) {
+      foreach ($environment['servers'] as $server => $server_env) {
      
         $aliases[$env_key . '.'. $server] = array(
-          'root' => '/var/www/drupal/' . $env_key . '/' . $deploy_sitename . '/current',
+          'root' => $server_env['deploy-to'] . '/' . $env_key . '/' . $deploy_sitename . '/current',
           'remote-user' => 'vagrant',
-				  'deploy-env-tag' => $env_key,
+	  'deploy-env-tag' => $env_key,
           'remote-host' => $server,
           'command-specific' => array(
             'deploy' => array(
                'application' => $deploy_sitename . $env_key,
                'deploy-repository' => $deploy_repository,
                'branch' => 'master',
-               'keep-releases' => 3,
-			     		 'deploy-via' => 'RemoteCache',
-               'deploy-to' => '/var/www/drupal/' . $env_key . '/' . $deploy_sitename,
+               'keep-releases' => $server_env['branch'],
+	       'deploy-via' => 'RemoteCache',
+               'deploy-to' => $server_env['deploy-to'] . '/' . $env_key . '/' . $deploy_sitename,
              )
           )
        );
