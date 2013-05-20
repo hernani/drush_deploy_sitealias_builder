de8af82192e554dd228b36d42f5f66c2e17fa0dc
root <root@debian.(none)>
2013-05-20 15:39:34 +0200

    Added web_listing

---
+++ web_listing.php
@@ -0,0 +1,89 @@
+<?php
+  /**
+   * This file lists the different site aliases defined and expose them to
+   * Jenkins
+   */
+
+  /**
+   * Auxiliary function to check end of string
+   */
+  function _endsWith($haystack,$needle,$case=true) {
+    if($case){return (strcmp(substr($haystack, strlen($haystack) - strlen($needle)),$needle)===0);}
+    return (strcasecmp(substr($haystack, strlen($haystack) - strlen($needle)),$needle)===0);
+  }
+
+  /**
+   * Auxiliary function to load all aliases files
+   */
+  function _loadAliasesFiles($dir) {
+
+    $aliases_files = $installed_sites = $all_aliases = array();
+
+    // load all files of a directory
+    foreach (new DirectoryIterator($dir) as $file) {
+      if ($file->isFile()) {
+          $file_name = $file->getFileName();
+          if (_endsWith($file_name, '.aliases.drushrc.php')) {
+            $aliases_files[] = $file_name;
+          }
+      }
+    }
+
+    // loop through the found files
+    if (!empty($aliases_files)) {
+      foreach ($aliases_files as $alias_file) {
+        // get the name of the site
+        $pos = strpos($alias_file,'.aliases.drushrc.php');
+        $site_name = substr($alias_file,0, $pos);
+        $installed_sites[$site_name] = $alias_file;
+        include ($dir . '/' . $alias_file);
+
+        // loop through the aliases
+        foreach ($aliases as $key => $value) {
+          $key_parts = explode(".", $key);
+
+          // not a valid alias
+          if (isset($key_parts[1]) && $key_parts[1] == $value['remote-host']) {
+            continue;
+          }
+
+          $all_aliases[$site_name][$key] = $value;
+        }
+      }
+    }
+
+    return $all_aliases;
+  }
+
+  // load files
+  $installed_sites = _loadAliasesFiles('/etc/drush');
+
+  // with multisites and all environments
+  if (isset($_GET['mode']) && $_GET['mode'] == 'full') {
+    foreach ($installed_sites as $project => $sites) {
+      foreach ($sites as $key => $value) {
+        $installed_sites_with_multisites[$project . '.' . $key] = $value;
+      }
+    }
+
+    ksort($installed_sites_with_multisites);
+    $installed_sites = $installed_sites_with_multisites;
+  }
+
+
+  // TODO
+  if (isset($_GET['display']) && $_GET['display'] == 'web') {
+    if (!empty($installed_sites)) {
+      print "sites=";
+      print "\n";
+    }
+  }
+  else {
+    // print them
+    if (!empty($installed_sites)) {
+      print "sites=";
+      print implode(',', array_keys($installed_sites));
+      print "\n";
+    }
+  }
+
